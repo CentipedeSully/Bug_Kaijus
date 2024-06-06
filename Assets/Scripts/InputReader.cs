6bee240b3f6a6f5442ab1ff4ffc6e4bc4d2e175e
William Sullivan-Smith <sullivansmith057@gmail.com>
2024-06-06 14:03:49 -0500

    implemented more bindings. Directional, Zoom, and cycling. Debugged the events that fire as a result of the input detections.

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -18,12 +18,18 @@ public class InputReader : MonoBehaviour, IDebugLoggable
 
     [SerializeField] private bool _showDebug = false;
 
-    [Header("Input Events")]
+    [Header("Selection Events")]
     [Space(5)]
     public UnityEvent OnSelectInput;
     public UnityEvent OnActionInput;
     public UnityEvent OnAuxInput;
     public UnityEvent OnBackInput;
+
+    [Header("Manipulation Events")]
+    [Space(5)]
+    public UnityEvent<Vector2> OnDirectionalInput;
+    public UnityEvent<int> OnZoomInput;
+    public UnityEvent<int> OnCycleInput;
 
 
 
@@ -77,7 +83,7 @@ public class InputReader : MonoBehaviour, IDebugLoggable
 
 
     //External
-    public void ReadSelectionInput(InputAction.CallbackContext context)
+    public void DetectSelectionInput(InputAction.CallbackContext context)
     {
         if (_isSelectInputReady && context.phase == InputActionPhase.Performed)
         {
@@ -89,7 +95,7 @@ public class InputReader : MonoBehaviour, IDebugLoggable
         }
     }
 
-    public void ReadActionInput(InputAction.CallbackContext context)
+    public void DetectActionInput(InputAction.CallbackContext context)
     {
         if (_isActionInputReady && context.phase == InputActionPhase.Performed)
         {
@@ -101,7 +107,7 @@ public class InputReader : MonoBehaviour, IDebugLoggable
         }
     }
 
-    public void ReadAuxillaryInput(InputAction.CallbackContext context)
+    public void DetectAuxillaryInput(InputAction.CallbackContext context)
     {
         if (_isAuxInputReady && context.phase == InputActionPhase.Performed)
         {
@@ -113,7 +119,7 @@ public class InputReader : MonoBehaviour, IDebugLoggable
         }
     }
 
-    public void ReadBackInput(InputAction.CallbackContext context)
+    public void DetectBackInput(InputAction.CallbackContext context)
     {
         if (_isBackInputReady && context.phase == InputActionPhase.Performed)
         {
@@ -126,7 +132,51 @@ public class InputReader : MonoBehaviour, IDebugLoggable
     }
 
 
+    public void DetectDirectionalInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed || context.action.IsPressed())
+        {
+            Vector2 directionalData = context.ReadValue<Vector2>();
+            OnDirectionalInput.Invoke(directionalData);
 
+            if (_showDebug)
+                LogDebug.Log($"Detected Directional Input: {directionalData}",this);
+        }
+    }
+
+    public void DetectZoomInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed || context.action.IsPressed())
+        {
+            //Mouse scroll is interpreted as the y value of a Vector2
+            float rawZoomInput = context.ReadValue<Vector2>().y;
+
+            //normalize the zoom input
+            if (rawZoomInput > 0)
+                rawZoomInput = 1;
+            else if (rawZoomInput < 0)
+                rawZoomInput = -1;
+
+            int zoomInput = (int)rawZoomInput;
+
+            OnZoomInput.Invoke((int)zoomInput);
+
+            if (_showDebug)
+                LogDebug.Log($"Detected Zoom Input: {zoomInput}", this);
+        }
+    }
+
+    public void DetectCycleInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed || context.action.IsPressed())
+        {
+            float cycleInput = context.ReadValue<float>();
+            OnCycleInput.Invoke((int)cycleInput);
+
+            if (_showDebug)
+                LogDebug.Log($"Detected Cycle Input: {(int)cycleInput}", this);
+        }
+    }
 
 
 
